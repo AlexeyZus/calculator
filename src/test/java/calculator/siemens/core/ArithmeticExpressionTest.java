75bfa7d6261ad6f01f3c0e8b16e3ad0a6faed869
alexey <alexeyzyuzin@gmail.com>
2016-04-28 13:19:42 +0300

    delete commented code

--- src/test/java/calculator/siemens/core/ArithmeticExpressionTest.java
+++ src/test/java/calculator/siemens/core/ArithmeticExpressionTest.java
@@ -74,45 +74,4 @@ public class ArithmeticExpressionTest {
         assertNotNull(error);
         assertEquals("Invalid set of elements: 1.+1",error);
     }
-
-
-
-
-
-    /*@Before
-    public void init(){ expression = new ArithmeticExpression();}
-
-    @After
-    public void delete(){ expression = null;}*/
-
-    /*@Test(expected = NullPointerException.class)
-    public void testSetArithmeticExpresionNull() throws Exception {
-        expression.setArithmeticExpresion(null);
-    }*/
-
-    /*@Before
-    public void init(){ expression = new ArithmeticExpression();}
-
-    @After
-    public void delete(){ expression = null;}*/
-
-
-    /*@Test
-    public void testSetArithmeticExpresion() throws Exception {
-        expression = new ArithmeticExpression();
-        expression.setArithmeticExpresion("5+5");
-        assertEquals("5+5",expression.getArithmeticExpresion());
-        expression = null;
-
-    }
-
-    public void testGetArithmeticExpresion() throws Exception {
-        expression = new ArithmeticExpression();
-        expression.setArithmeticExpresion("5+5");
-        assertEquals("5+5",expression.getArithmeticExpresion());
-        expression = null;
-
-    }
-*/
-
 }
